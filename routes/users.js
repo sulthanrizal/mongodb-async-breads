8576cb64cec7eee527a79951bb96201daae2e7a2
sulthanrizal <surie0312@gmail.com>
2023-12-21 09:20:30 +0700

    create router add

--- routes/users.js
+++ routes/users.js
@@ -9,27 +9,54 @@ module.exports = function (db) {
   const User = db.collection('users')
   router.get('/', async (req, res, next) => {
     try {
-      const { page = 1, sortBy = '_id', sortMode = 'desc', limit = '5', query = '' } = req.query
-      const offset = (page - 1) * limit
+      const { page = 1, limit = '5', query = '', sortBy = '_id', sortMode = 'desc' } = req.query
       const params = {}
       const sort = {}
       sort[sortBy] = sortMode
+      const offset = (page - 1) * limit
+
       if (query) {
         params['$or'] = [{ "name": new RegExp(query, 'i') }, { "phone": new RegExp(query, 'i') }]
+        console.log(params)
       }
+
       const total = await User.count(params)
       const pages = Math.ceil(total / limit)
+
       const users = await User.find(params).sort(sort).limit(Number(limit)).skip(Number(offset)).toArray()
       res.json({ data: users, total, pages, page: (Number(page)), limit: (Number(limit)), offset });
+
     } catch (err) {
       res.status(500).json({ err })
     }
 
   });
 
-  // router.post('/', async (req, res, next) => {
 
-  // })
+  router.post('/', async (req, res, next) => {
+    try {
+      const { name, phone } = req.body
+      const user = await User.insertOne({ name: name, phone: phone })
+      res.status(201).json(user)
+    } catch (err) {
+      res.status(500).json({ err })
+    }
+  })
+
+
+
+
+
+
+  router.delete('/:id', async (req, res, next) => {
+    try {
+      const id = req.params.id
+      const user = await User.findOneAndDelete({ _id: new ObjectId(id) })
+      res.status(201).json(user)
+    } catch (err) {
+      res.status(500).json({ err })
+    }
+  })
 
 
 
