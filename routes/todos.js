68bbe15957f42d37a2468cad0316d6b19b20c11c
sulthanrizal <surie0312@gmail.com>
2023-12-25 18:56:37 +0700

    create routers todos

--- routes/todos.js
+++ routes/todos.js
@@ -10,17 +10,40 @@ module.exports = function (db) {
 
     router.get('/', async (req, res, next) => {
         try {
-            const { page = 1, limit = 10, startdateDeadline, enddateDeadline, complete, sortMode = 'desc', sortBy = '_id', executor } = req.query
+            const { page = 1, limit = 10, title, startdateDeadline, enddateDeadline, complete, sortMode = 'desc', sortBy = '_id', executor } = req.query
             const params = {}
             const sort = {}
             sort[sortBy] = sortMode
-            const offset = (page - 1)
+            const offset = (page - 1) * limit
 
-            const todo = await Todo.find(params).toArray()
+            if (title) {
+                params['title'] = new RegExp(title, 'i')
+            }
+
+            if (startdateDeadline && enddateDeadline) {
+                params['deadline'] = {
+                    $gte: new Date(startdateDeadline),
+                    $lte: new Date(enddateDeadline)
+                }
+            } else if (startdateDeadline) {
+                params['deadline'] = { $gte: new Date(startdateDeadline) }
+            } else if (enddateDeadline) {
+                params['deadline'] = { $lte: new Date(enddateDeadline) }
+            }
+
+            if (complete) {
+                params['complete'] = JSON.parse(complete)
+            }
+
+            if (executor) {
+                params['executor'] = new ObjectId(executor)
+            }
+
 
             const total = await Todo.count(params)
             const pages = Math.ceil(total / limit)
 
+            const todo = await Todo.find(params).sort(sort).limit(Number(limit)).skip(Number(offset)).toArray();
             res.status(201).json({ data: todo, total, pages, page: Number(page), limit })
         } catch (err) {
             res.status(500).json({ err })
@@ -35,6 +58,16 @@ module.exports = function (db) {
             const todo = await Todo.insertOne({ title: title, complete: false, deadline: new Date(Date.now() + oneDay), executor: user._id })
             const data = await Todo.find({ _id: new ObjectId(todo.insertedId) }).toArray()
             res.status(201).json(data)
+        } catch (err) {
+            res.status(500).json({ err })
+        }
+    })
+
+    router.get('/:id', async (req, res, next) => {
+        try {
+            const id = req.params.id
+            const todo = await Todo.findOne({ _id: new ObjectId(id) })
+            res.status(201).json(todo)
         } catch (err) {
             res.status(500).json({ err })
         }
