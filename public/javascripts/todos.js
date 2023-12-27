41e13a705efab4d762f5367d1be96ec750251c1a
sulthanrizal <surie0312@gmail.com>
2023-12-27 13:51:30 +0700

    save data Jquery

--- public/javascripts/todos.js
+++ public/javascripts/todos.js
@@ -1,4 +1,5 @@
-//variables support
+
+// variables support
 let id = null, title = '', complete = '', page = 1, limit = 10, sortBy = '_id', sortMode = 'desc'
 let deadline = '', startdateDeadline = '', enddateDeadline = ''
 let complt = false
@@ -11,24 +12,44 @@ function getId(id) {
 
 
 
-const readData = async () => {
-    const readData = async function () {
-        let pagination = ''
-        let pageNumber = ''
-        try {
-            const response = await $.ajax({
-                url: `http://localhost:3000/api/users?query=${query}&page=${page}&limit=${limit}&sortBy=${sortBy}&sortMode=${sortMode}`,
-                method: "GET",
-                data: { id: menuId },
-                dataType: "json"
-            })
-            const users = await response.json();
+const readData = async (semua) => {
+    try {
+        const response = await $.ajax({
+            url: `/api/todos`,
+            method: "GET",
+            dataType: "json",
+            data: {
+                executor,
+                sortBy,
+                sortMode,
+                deadline,
+                title,
+                startdateDeadline,
+                enddateDeadline,
+                complete,
+                page,
+                limit
+            }
+        })
+        let htmlList = ''
+        response.data.forEach((item, index) => {
+            htmlList += ` 
+                <div id="${item._id}" class="todoslist ${item.complete == false && new Date(`${item.deadline}`).getTime() < new Date().getTime() ? ' alert alert-danger' : item.complete == true ? ' alert alert-success' : ' alert alert-secondary'}" role="alert">
+                 ${moment(item.deadline).format('DD-MM-YYYY HH:mm')} ${item.title}
+                </div>
+                `
+        })
 
-        } catch (err) {
-            res.status(500).json({ err })
+        if (semua == false) {
+            $('#showTodos').append(htmlList)
+        } else if (semua == true) {
+            $('#showTodos').html(htmlList)
         }
+
+    } catch (err) {
+        console.log(err)
     }
-    readData()
 }
+readData(complt)
 
 
